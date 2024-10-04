ff46d0b29e949862f42ea15be7c3aa2a9a96db5b
wonsungjin <111822969+wonsungjin@users.noreply.github.com>
2024-10-04 16:05:11 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = numbers;
+        for(int i=0;i<numbers.length;i++) numbers[i]*=2;
+        return answer;
+    }
+}
