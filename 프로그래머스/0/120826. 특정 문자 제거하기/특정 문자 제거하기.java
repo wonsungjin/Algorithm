8b595e7f9bcca98d6463952a376b4a9f71043727
wonsungjin <111822969+wonsungjin@users.noreply.github.com>
2024-10-21 22:01:32 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 31.41 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for(int i =0;i<my_string.length;i++)
+        {
+            if(!my_string.charAt(i).equals(letter)) answer+=my_string.charAt(i);
+        }
+        return answer;
+    }
+}
